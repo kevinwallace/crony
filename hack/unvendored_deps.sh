2feaf69a13a53a8ce30caecba8f13b8ee38f4b71
Kevin Wallace <kevin@pentabarf.net>
2014-01-10 15:47:45 -0800

    Make unvendored_deps.sh more robust:
    * Search packages in sub-directories of the repo root.
    * Don't list the current package or its sub-packages as un-vendored.
    
    (Crony doesn't currently have any non-main packages, so this is a no-op for now)

--- hack/unvendored_deps.sh
+++ hack/unvendored_deps.sh
@@ -2,11 +2,16 @@
 
 cd "$(dirname "$0")/.."
 
+local_package="$(go list)"
+
 function list_external_packages() {
-  go list -f '{{join .Deps "\n"}}' | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}'
+  go list -f '{{join .Deps "\n"}}' ./... | xargs go list -f '{{if not .Standard}}{{.ImportPath}}{{end}}'
 }
 
 list_external_packages | while read package; do
+  if [[ "${package}" == "${local_package}/"* ]]; then
+    continue
+  fi
   if [ ! -d "vendor/src/${package}" ]; then
     echo "${package}"
   fi
